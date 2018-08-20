44eb68c8228159d604c93a16c5ff94ca2821f4e4
lmxdawn@qq.com <lmxdawn@qq.com>
2018-08-20 13:39:27 +0800

    request 封装

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 //app.js
 
-var appSelf = undefined;
+let appSelf = undefined;
 
 App({
     onLaunch: function () {
     },
 
-
-    // 微信小程序后台登录
-    applyLogin: function (callback) {
-        var self = this
-
-
-
-    },
-
-
     /*基础配置加载、弹框等*/
     globalData: {
         userInfo: null
     }
-})
+});
